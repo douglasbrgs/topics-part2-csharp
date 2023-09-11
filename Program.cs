c9ad24029920870d09c64820b9339e76f2fb09e5
Douglas Borges <douglasborges19@gmail.com>
2023-09-11 19:32:21 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Course01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date = new DateTime(2023, 8, 12, 11, 30, 15);
+            Console.WriteLine(date.ElapsedTime());
+
+            DateTime date2 = new DateTime(2023, 9, 11, 13, 23, 20);
+            Console.WriteLine(date2.ElapsedTime());
+
+            String message1 = "Good morning dear students!";
+            Console.WriteLine(message1.Cut(10));
+        }
+    }
+}
